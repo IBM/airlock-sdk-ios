801e529a728616dd2d7783ac571fbbb6270d0bce
Yoav Ben-Yair <yoavbe@il.ibm.com>
2021-06-20 16:58:13 +0300

    SPM name fix

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "airlock-sdk-ios-ibm",
+    name: "AirLockSDK",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "airlock-sdk-ios-ibm",
-            targets: ["airlock-sdk-ios-ibm"]),
+            name: "AirLockSDK",
+            targets: ["AirLockSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "airlock-sdk-ios-ibm",
+            name: "AirLockSDK",
             dependencies: []),
         .testTarget(
             name: "airlock-sdk-ios-ibmTests",
-            dependencies: ["airlock-sdk-ios-ibm"]),
+            dependencies: ["AirLockSDK"]),
     ]
 )
